f36581610fcf57c8d677b59d662124285250b1a7
wuwanping <1784691647@qq.com>
2020-06-17 09:50:58 +0800

    更新

---
+++ src/views/bayonet/middle.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="middle">
+      <div @click="change(1)">
+         <m-title  label='卡口热力分布' class="car"></m-title> 
+      </div>
+      <div @click="change(2)">
+        <m-title  label='活跃卡口点位' class="car"></m-title> 
+      </div> 
+  </div>
+</template>
+
+<script>
+import mTitle from "@/components/UI_el/title_com.vue";
+import mTab from '@/components/UI_el/tab.vue';
+import blur from '../../blur.js';
+export default {
+    name:'middle',
+    data(){
+        return{
+
+        }
+    },
+    components:{
+        mTitle,blur,mTab
+    },
+    methods:{
+        // 切换卡口热力分布  与   活跃卡口点位
+        change(i){   
+            alert(i)
+        //   blur.$emit('realtime',i)
+        },
+    },
+    mounted(){
+
+    }
+}
+</script>
+
+<style scope lang='scss'>
+
+.middle{
+    position: fixed;
+    top:95.6vh;
+    left:756px;
+    width: 38.85vw;
+    display: flex;
+    .car{
+        width:199px;
+        height:39px;
+        justify-content: space-around;
+        line-height: 39px;
+        cursor:pointer;
+    }
+}
+</style>
