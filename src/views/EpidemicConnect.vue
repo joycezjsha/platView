e6cfc060e793cd8fbc3a30017899fb86fc349378
joyce <zhoujing@cennavi.com.cn>
2020-07-16 10:50:51 +0800

    no message

---
+++ src/views/EpidemicConnect.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="iframe">
+    <iframe id="example" :src="http" frameborder="no" border="0" name='name'></iframe>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "example-connect",
+        data(){
+          return {
+            http:urlConf.YQ_PATH
+          }
+        }
+    }
+</script>
+
+<style scoped>
+  .iframe {
+    width:100%;
+    height:100%;
+  }
+  #example {
+    width:100%;
+    height:100%;
+  }
+  .active-menu, .active-menu i {
+    background-color: #1B1B1B !important;
+    color: #F09B22 !important;
+}
+.active-menu i{color: #F09B22 !important;}
+.arrow {
+    float: right;
+    margin-top: 8px;
+}
+</style>
